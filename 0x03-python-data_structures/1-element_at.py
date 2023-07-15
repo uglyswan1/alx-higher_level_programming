59c10fdabb0eeaa1da46aa9d64881f60906a15ce
uglyswan1 <knza.hrsn.14@gmail.com>
2023-07-15 16:55:23 -0700

    0x03 py

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def element_at(my_list, idx):
+    """Retrieves an element from a list"""
+    if idx < 0
+        print("None")
+    elif idx >= len(my_list)
+        print ("None")
+    else
+        print (my_list["{:d}".format(my_list[idx])])
